cac3fcd5f76571f768440ba421a076bde6ca48a5
nervachan <nervagrf@gmail.com>
2026-02-02 07:13:30 +0000

    added scene 12,13,14,15

--- src/game/scenes/Scene11.ts
+++ src/game/scenes/Scene11.ts
@@ -14,10 +14,11 @@ export class Scene11 extends Scene {
 		this.background.setDisplaySize(1600, 900);
         this.background.setDepth(0);
 
-		// Debug: log background clicks
+		// Debug: log background clicks and transition to Scene12
 		this.background.setInteractive();
 		this.background.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
 			console.log(`[Scene11] Background click screen=(${pointer.x},${pointer.y}) world=(${pointer.worldX},${pointer.worldY})`);
+			this.scene.start('Scene12');
 		});
 
 		EventBus.emit('current-scene-ready', this);
--- src/game/scenes/Scene15.ts
+++ src/game/scenes/Scene15.ts
@@ -4,19 +4,47 @@ import { EventBus } from '../EventBus';
 
 export class Scene15 extends Scene {
 	background!: GameObjects.Image;
+	textBox!: GameObjects.Text;
+	flashOverlay!: GameObjects.Rectangle;
 
 	constructor() {
 		super('Scene15');
 	}
 
 	create() {
+		// Background that fits the screen
 		this.background = this.add.image(800, 450, 'scene_15');
+		this.background.setDisplaySize(1600, 900);
 		this.background.setDepth(0);
 
-		// Debug: log background clicks
-		this.background.setInteractive();
-		this.background.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-			console.log(`[Scene15] Background click screen=(${pointer.x},${pointer.y}) world=(${pointer.worldX},${pointer.worldY})`);
+		// Flash effect on scene start
+		this.flashOverlay = this.add.rectangle(800, 450, 1600, 900, 0xffffff);
+		this.flashOverlay.setDepth(100);
+		this.tweens.add({
+			targets: this.flashOverlay,
+			alpha: { from: 1, to: 0 },
+			duration: 300,
+			ease: 'Power2',
+			onComplete: () => {
+				this.flashOverlay.destroy();
+			}
+		});
+
+		// Textbox with documentation message
+		this.textBox = this.add.text(800, 800, 'technician photo documentation in progress', {
+			fontSize: '32px',
+			color: '#ffffff',
+			backgroundColor: '#000000',
+			padding: { x: 20, y: 10 },
+			align: 'center'
+		});
+		this.textBox.setOrigin(0.5, 0.5);
+		this.textBox.setDepth(10);
+
+		// Click listener for navigation and logging
+		this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+			console.log(`[INPUT] click screen=(${pointer.x},${pointer.y}) world=(${pointer.worldX},${pointer.worldY})`);
+			this.scene.start('Scene1');
 		});
 
 		EventBus.emit('current-scene-ready', this);
